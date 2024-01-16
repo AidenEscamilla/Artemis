b63beb065d0d5daebb575a01793efeeb405bc36f
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-01-16 16:58:57 -0600

    added articles page & model

--- job_tracker/config/routes.rb
+++ job_tracker/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  # TODO: Learn to make routes look like this
+  # resource :basket, only: [:show, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
