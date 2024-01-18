0cd6ba516d7cbcb43e5adf31241056f29162556d
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-01-18 15:22:41 -0600

    added Edit & update, use partials & link_to

--- job_tracker/config/routes.rb
+++ job_tracker/config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  # TODO: Learn to make routes look like this
-  # resource :basket, only: [:show, :update, :destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :articles
 end
