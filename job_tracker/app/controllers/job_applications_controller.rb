8b3f92f68ded3bdc3b98be73c8a0def908c83a63
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-01-31 16:01:02 -0600

    Update: Wired up controller, fixed index display

--- job_tracker/app/controllers/job_applications_controller.rb
+++ job_tracker/app/controllers/job_applications_controller.rb
@@ -23,27 +23,19 @@ class JobApplicationsController < ApplicationController
   def create
     @job_application = JobApplication.new(job_application_params)
 
-    respond_to do |format|
-      if @job_application.save
-        format.html { redirect_to job_application_url(@job_application), notice: "Job application was successfully created." }
-        format.json { render :show, status: :created, location: @job_application }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @job_application.errors, status: :unprocessable_entity }
-      end
+    if @job_application.save
+      redirect_to job_applications_path, notice: "Job application was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /job_applications/1 or /job_applications/1.json
   def update
-    respond_to do |format|
-      if @job_application.update(job_application_params)
-        format.html { redirect_to job_application_url(@job_application), notice: "Job application was successfully updated." }
-        format.json { render :show, status: :ok, location: @job_application }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @job_application.errors, status: :unprocessable_entity }
-      end
+    if @job_application.update(job_application_params)
+      redirect_to job_applications_path, notice: "Job application was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
