b5ef6eac7afe97bc78b07d9b212b32357aa9fae9
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-01-31 17:22:04 -0600

    Added Bootstrap (fingers crossed no more errors)

--- job_tracker/app/javascript/application.js
+++ job_tracker/app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import { application } from "./application";
+import * as bootstrap from "bootstrap";
+// Rails.start();
+
