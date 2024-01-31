15f4932d50363b2ee4970ce1a944682de3fe8de5
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-01-30 22:07:46 -0600

    Added job_application status

--- job_tracker/app/models/job_application.rb
+++ job_tracker/app/models/job_application.rb
@@ -1,3 +1,6 @@
 class JobApplication < ApplicationRecord
   belongs_to :employer
+
+  enum status: %i[applied rejected interview_scheduled second_round waiting_decision offered_job hired].index_with(&:to_s)
+
 end
--- job_tracker/db/migrate/20240120224916_create_job_applications.rb
+++ job_tracker/db/migrate/20240120224916_create_job_applications.rb
@@ -1,11 +1,9 @@
 class CreateJobApplications < ActiveRecord::Migration[7.1]
   def change
-    create_enum :status, %i[applied rejected interview_scheduled second_round waiting_decision offered_job hired]
-
     create_table :job_applications, id: :uuid do |t|
       t.references :employer, null: false, foreign_key: true, type: :uuid
       t.string :title
-      t.enum :status, default: :applied, null: false, enum_type: "status"
+      t.string :status, default: 'applied', null: false
 
       t.timestamps
     end
