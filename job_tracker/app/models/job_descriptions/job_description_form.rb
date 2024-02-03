46465dd7f8bb682f20823840a7ef2e2ec3b7cde8
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-02-02 18:30:10 -0600

    Fixed: Copied validation to form object

--- job_tracker/app/models/job_descriptions/job_description_form.rb
+++ job_tracker/app/models/job_descriptions/job_description_form.rb
@@ -7,8 +7,12 @@ class JobDescriptions::JobDescriptionForm
   validates :job_description, :description, :requirements, 
     :job_classification, :min_salary, :max_salary, :job_application_id, presence: true
   
-  validates :job_classification, format: { with: /\A[a-zA-Z\s]+\z/ }
+    validates :job_classification, 
+    format: { with: /\A[a-zA-Z\s]+\z/ },  unless: -> { job_classification.blank? }
+
   validates :min_salary, :max_salary, format: { with: /\A\d+\z/ }
+  validate :min_salary_less_than_max_salary
+
 
   def initialize(job_description:)
     @job_description = job_description
@@ -39,4 +43,13 @@ class JobDescriptions::JobDescriptionForm
     end
   end
 
+  private
+
+  def min_salary_less_than_max_salary
+    return unless min_salary.present? && max_salary.present?
+
+    if min_salary > max_salary
+      errors.add(:min_salary, 'must be less than max salary')
+    end
+  end
 end
