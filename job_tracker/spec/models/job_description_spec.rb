798488c9bc7868f0df47809ca69dfd3c57b75e1d
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-02-05 16:22:32 -0600

    Added: Model unit test

--- job_tracker/spec/models/job_description_spec.rb
+++ job_tracker/spec/models/job_description_spec.rb
@@ -5,9 +5,17 @@ RSpec.describe JobDescription, type: :model do
   let!(:job_description) { create(:job_description, job_application:)  }
 
   describe 'validations' do
-    context 'when all attributes are valid' do
-      it 'is valid' do
-        p job_description
+    context 'when created with job_application' do
+      it 'can be saved' do
+        expect(job_description).to be_valid
+      end
+    end
+
+    context 'when created without job_application' do
+      before { job_description.job_application = nil }
+
+      it 'cannot be saved' do
+        expect(job_description).to_not be_valid
       end
     end
   end
