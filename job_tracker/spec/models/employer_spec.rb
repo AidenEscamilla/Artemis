9dfefc9238a424dedb1d642df00891fbf7290634
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-02-05 21:39:13 -0600

    Added: inital employer model spec

--- job_tracker/spec/models/employer_spec.rb
+++ job_tracker/spec/models/employer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Employer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:employer) { Employer.new(name: "Test Employer") }
+
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(employer).to be_valid
+    end
+  end  
 end
