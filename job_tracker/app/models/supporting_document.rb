5f63cadd678d05ed8b369b6e49b43a871bb0b1db
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-02-09 15:06:45 -0600

    Added: model validations & specs

--- job_tracker/app/models/supporting_document.rb
+++ job_tracker/app/models/supporting_document.rb
@@ -1,4 +1,7 @@
 class SupportingDocument < ApplicationRecord
   belongs_to :job_application
   has_one_attached :document, dependent: :destroy
+
+  validates :name, :document, presence: true
+  validates :name, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
 end
